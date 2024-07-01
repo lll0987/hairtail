1fa4381785a966ef044119a4a2e0df3dfebc60f7
lll0987 <614204826@qq.com>
2024-07-01 23:17:00 +0800

    ✨ feat: 增加页面切换

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="w-screen h-screen">
-        <calendar :data="data"></calendar>
+    <div
+        class="w-screen h-screen grid grid-cols-[auto_1fr] grid-rows-1 bg-neutral-100 text-zinc-950"
+    >
+        <MainMenu v-model:actived="actived" :routes="routes"></MainMenu>
+        <div class="w-full h-full border-l-2 border-zinc-950">
+            <component :is="activedComponent" />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import calendar from './components/calendar';
+import { computed, ref } from 'vue';
+import MainMenu from './components/menu/menu.vue';
+import routes from './router/routes';
 
-import data from './data';
+const actived = ref('input');
+const activedComponent = computed(() => routes.find(i => i.key === actived.value)?.component);
 </script>
-
-<style></style>
