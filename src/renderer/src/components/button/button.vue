a6ed421bf705eadda6bc9b8213b7ab35e3d9bcce
lll0987 <614204826@qq.com>
2024-07-01 23:06:20 +0800

    ✨ feat: 增加 button 组件

---
+++ src/renderer/src/components/button/button.vue
@@ -0,0 +1,9 @@
+<template>
+    <button
+        class="bg-cyan-800 text-white border-2 border-zinc-950 rounded px-8 h-32 inline-flex flex-row justify-center items-center"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts"></script>
