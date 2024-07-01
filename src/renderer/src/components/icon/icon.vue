89223ffb5584a34e0cf5c7de03f3a78b856084e3
lll0987 <614204826@qq.com>
2024-07-01 22:35:35 +0800

    ✨ feat: 增加 icon 组件

---
+++ src/renderer/src/components/icon/icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <i class="w-[1em] h-[1em] leading-[1em] text-center inline-block" :style="styles">
+        <slot></slot>
+    </i>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{ size: number | string; color?: string }>();
+
+const styles = computed(() => ({
+    'font-size': typeof props.size === 'string' ? props.size : `${props.size}px`
+}));
+</script>
