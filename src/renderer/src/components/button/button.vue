2538ed6f140246f677654a9d1cd7dedb24ad7e1c
lll0987 <614204826@qq.com>
2024-07-04 16:35:28 +0800

    ✨ feat: 修改主要按钮背景色

--- src/renderer/src/components/button/button.vue
+++ src/renderer/src/components/button/button.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="bg-cyan-800 text-white border-2 border-zinc-950 rounded px-8 h-32 inline-flex flex-row justify-center items-center"
+        class="bg-pink-900 text-white border-2 border-zinc-950 rounded px-8 h-32 inline-flex flex-row justify-center items-center"
     >
         <slot></slot>
     </button>
