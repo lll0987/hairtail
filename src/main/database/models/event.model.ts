57c0ef643ae7bd30364866627d441fac3dc38419
lll0987 <614204826@qq.com>
2024-07-07 18:28:16 +0800

    ✨ feat(数据库): 修改 event 字段类型

--- src/main/database/models/event.model.ts
+++ src/main/database/models/event.model.ts
@@ -1,5 +1,5 @@
 import { model as Model, Schema } from 'mongoose';
-import { infoName, tagName, topicName } from '.';
+import { infoName } from '.';
 
 export const name = 'Event';
 
@@ -12,7 +12,7 @@ const schema = new Schema(
         // 颗粒度
         grain: { type: Number, require: true },
         // 主题
-        topic: { type: Schema.Types.ObjectId, ref: topicName, require: true },
+        topic: { type: String, require: true },
         // 标题
         title: { type: String, required: false },
         // 值
@@ -20,7 +20,7 @@ const schema = new Schema(
         // 备注
         remark: { type: String, required: false },
         // 标签
-        tags: [{ type: Schema.Types.ObjectId, ref: tagName, required: false }],
+        tags: [{ type: String, required: false }],
         // 关联信息
         infos: [{ type: Schema.Types.ObjectId, ref: infoName, required: false }]
     },
