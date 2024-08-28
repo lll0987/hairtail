ef2d332181c2743c1dacc93ea41770627044a8ef
lll0987 <614204826@qq.com>
2024-08-28 22:10:00 +0800

    feat: 增加菜单

--- src/renderer/src/router/routes.ts
+++ src/renderer/src/router/routes.ts
@@ -1,14 +1,36 @@
 import type { IRoute } from '@contracts/interface';
 
-import { IconHomeFilled } from '@tabler/icons-vue';
+import { IconCirclePlusFilled, IconHomeFilled, IconLayoutFilled } from '@tabler/icons-vue';
+import IconCalendarWeekFilled from './icons/IconCalendarWeekFilled.vue';
 
 import preview from '@renderer/views/preview';
+import today from '@renderer/views/today';
+import chat from '@renderer/views/chat';
+import calendar from '@renderer/views/calendar';
 
 export default [
     {
+        key: 'today',
+        label: '今日关注点',
+        icon: IconHomeFilled,
+        component: today
+    },
+    {
+        key: 'chat',
+        label: '记录',
+        icon: IconCirclePlusFilled,
+        component: chat
+    },
+    {
+        key: 'calendar',
+        label: '日历',
+        icon: IconCalendarWeekFilled,
+        component: calendar
+    },
+    {
         key: 'preview',
         label: '组件预览',
-        icon: IconHomeFilled,
+        icon: IconLayoutFilled,
         component: preview
     }
 ] as IRoute[];
