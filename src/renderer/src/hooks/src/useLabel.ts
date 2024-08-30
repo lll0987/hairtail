e1cdea4f34557268ce093e05e48065e0340ee852
lll0987 <614204826@qq.com>
2024-08-30 23:28:44 +0800

    feat: 隐藏 label

--- src/renderer/src/hooks/src/useLabel.ts
+++ src/renderer/src/hooks/src/useLabel.ts
@@ -1,23 +1,20 @@
-import { computed, toRef } from 'vue';
+import { computed, toRefs } from 'vue';
 
 export interface ILabelProps {
-    label?: string;
+    label?: string | false;
     placeholder?: string;
-    'show-label': boolean;
 }
 
 export const useLabel = (props: ILabelProps, def_label: string, def_placeholder: string) => {
-    const label = toRef(props, 'label');
-    const showLabel = toRef(props, 'show-label');
-    const placeholder = toRef(props, 'placeholder');
+    const { label, placeholder } = toRefs(props);
 
     const mergedLabel = computed(() => {
-        if (!showLabel.value) return '';
-        return label.value === undefined ? def_label : label.value;
+        if (label?.value === false) return '';
+        return label?.value === undefined ? def_label : label.value;
     });
 
     const mergedPlaceholder = computed(() => {
-        if (placeholder.value !== undefined) return placeholder.value;
+        if (placeholder?.value !== undefined) return placeholder.value;
         if (mergedLabel.value === def_label) return def_placeholder;
         return def_placeholder + mergedLabel.value;
     });
